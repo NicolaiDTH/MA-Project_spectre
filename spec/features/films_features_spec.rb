30752958b2939c4194f0448a87b805ff9c36a039
Nicolai <nicolai@mmg.io>
2014-07-22 13:16:45 +0100

    Tests rewritten

--- spec/features/films_features_spec.rb
+++ spec/features/films_features_spec.rb
@@ -1,31 +1,20 @@
 require 'rails_helper'
 require 'imdb'
 
-# describe 'spectre first page' do
-#  context 'has no films yet' do
-#   it 'asks a user to enter a film' do
-#    visit '/films'
-#    expect(page).to have_content 'enter a film'
-#   end
-#  end
-# end
-
-# describe 'spectre home page' do
-#  context 'has films' do
-#   it 'shows the added film' do
-#    visit '/films'
-#    expect(page).to have_content
-#   end
-#  end
-# end
-
-describe 'spectre main page' do
- context 'searching for films' do
+describe 'spectre beginning page' do
+ context 'shelf tool' do
   it 'searches for a film' do
    visit '/films'
    fill_in 'search_search', with: 'Vanilla Sky'
-   click_button 'search'
-   expect(page).to have_content 'Vanilla Sky'
+   click_button '+'
+   expect(page).to have_css("img[src*='http://ia.media-imdb.com/images/']")
+  end
+
+  it 'goes away when a film has been added' do
+   visit '/films'
+   fill_in 'search_search', with: 'Finding Nemo'
+   click_button '+'
+   expect(page).not_to have_content 'add a film'
   end
  end
 end
