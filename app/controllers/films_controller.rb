d35e4444527e9302042a5953aaa092c42bc8b0d4
Nicolai <nicolai@mmg.io>
2014-07-28 18:58:13 +0100

    Database Issues

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -4,12 +4,12 @@ class FilmsController < ApplicationController
  before_action :authenticate_user!
 
  def index
-  if Films.any?
+  if current_user.films.any?
   
    #Makes sure that the film hasn't been seen entered into the database before 
 
    #Finds the actor ids from a random shelf movies
-   shelfactor = Films.where(status: 'shelved').sample.actors.split(",").sample
+   shelfactor = current_user.films.where(status: 'shelved').sample.actors.split(",").sample
    
    #Finds a randomised movies from the randomised actor
    movie_id =  Imdb::Search.new(shelfactor).movies.shuffle.detect {|movie| movie.length && movie.length > 65 }.id
@@ -18,14 +18,14 @@ class FilmsController < ApplicationController
    movie = Imdb::Movie.new(movie_id)
    
    #Creates the database element of the recommendation
-   @suggestion = Films.create(:title => movie.title, :movie_id => movie.id, :year => movie.year, :runtime => movie.length, :ratings => movie.rating, :votes => movie.votes, :poster => movie.poster, :actors => movie.cast_member_ids.take(5).join(","), :status => 'suggested')
+   @suggestion = current_user.films.create(:title => movie.title, :movie_id => movie.id, :year => movie.year, :runtime => movie.length, :ratings => movie.rating, :votes => movie.votes, :poster => movie.poster, :actors => movie.cast_member_ids.take(5).join(","), :status => 'suggested')
 
-   if Films.find_by(movie_id: @suggestion.id)
+   if current_user.films.find_by(movie_id: @suggestion.id)
     redirect_to '/films'
    end
   end
-  @shelf = Films.where(status: 'shelved')
-  @list = Films.where(status: 'listed')
+  @shelf = current_user.films.where(status: 'shelved')
+  @list = current_user.films.where(status: 'listed')
  end
 
  def update
@@ -41,7 +41,7 @@ class FilmsController < ApplicationController
   search = Imdb::Search.new(title)
   movie_id = search.movies.first.id
   movie = Imdb::Movie.new(movie_id)
-  @films = Films.create(:title => movie.title, :movie_id => movie.id, :year => movie.year, :runtime => movie.length, :ratings => movie.rating, :votes => movie.votes, :poster => movie.poster, :actors => movie.cast_member_ids.take(5).join(","), :status => 'shelved')
+  @films = current_user.films.create(:title => movie.title, :movie_id => movie.id, :year => movie.year, :runtime => movie.length, :ratings => movie.rating, :votes => movie.votes, :poster => movie.poster, :actors => movie.cast_member_ids.take(5).join(","), :status => 'shelved')
   redirect_to '/films'
  end
 
--- app/models/films.rb
+++ app/models/films.rb
@@ -1,3 +1,9 @@
 class Films < ActiveRecord::Base
- has_many :users
+ belongs_to :user
 end
+
+
+# id | title | user_id | status
+# 1    Memento  1        shelved
+# 2    Titanic  2        shelved
+# 3    Memento  2        rejected
